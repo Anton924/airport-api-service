8dacc100f29d3ca8d312877c93c107af5ea78065
Anton <yakovenkoanton2007@gmail.com>
2026-06-25 18:03:06 +0200

    feat: Add user registration, permissions and JWT auth

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from users.views import UserCreate, ManageUserView
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("register/", UserCreate.as_view(), name="register"),
+    path("me/", ManageUserView.as_view(), name="user_info")
+]
+
+
+app_name="user"
