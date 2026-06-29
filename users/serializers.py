f4001bc596469e775c8dcb357456d4766ac4fedd
Anton <yakovenkoanton2007@gmail.com>
2026-06-29 10:15:30 +0200

    feat: Update user serializer with password hashing

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,25 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+)
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
-
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5,
+                "label": _("Password"),
+                "trim_whitespace": False,
+                "style": {"input_type": "password"},
+            },
+        }
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
